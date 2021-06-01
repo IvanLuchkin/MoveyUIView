5ead6a783761e7e7fa3325fe29dcb0850ae4ec51
Yaroslav <Stamin4ik4@gmail.com>
2021-06-01 18:21:22 +0300

    added image to profile and movie cards

--- app/src/main/java/com/example/moveyuiview/MovieCardsAdapter.java
+++ app/src/main/java/com/example/moveyuiview/MovieCardsAdapter.java
@@ -2,6 +2,8 @@ package com.example.moveyuiview;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +12,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MovieCardsAdapter extends ArrayAdapter<MovieCard> {
 
@@ -28,9 +38,46 @@ public class MovieCardsAdapter extends ArrayAdapter<MovieCard> {
         }
         TextView title = convertView.findViewById(R.id.title);
         ImageView image = convertView.findViewById(R.id.movieImageView);
-        SuggestionsActivity.LoadImage(movieCardItem.getImageUrl(),image);
+        LoadImage(movieCardItem.getImageUrl(),image);
         title.setText(movieCardItem.getMovieTitle());
-        image.setImageResource(R.mipmap.ic_launcher);
         return convertView;
     }
+
+    public static void LoadImage(String url, ImageView imageView){
+        ImageViewHelper viewHelper = new ImageViewHelper();
+        try {
+            Bitmap bitmap = viewHelper.execute(url).get();
+            imageView.setImageBitmap(bitmap);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class ImageViewHelper extends AsyncTask<String,Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            Bitmap bitmap = null;
+            URL url ;
+            HttpURLConnection httpURLConnection;
+            InputStream inputStream;
+
+            try{
+                url = new URL(strings[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                inputStream=httpURLConnection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+
+            }catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return  bitmap;
+        }
+    }
+
 }
--- app/src/main/java/com/example/moveyuiview/ProfileActivity.java
+++ app/src/main/java/com/example/moveyuiview/ProfileActivity.java
@@ -6,19 +6,33 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private TextView username;
     private Button logout;
+    private ImageView userImage;
     private SharedPreferences prefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,8 @@ public class ProfileActivity extends AppCompatActivity {
         username = findViewById(R.id.profile_user);
         username.setText(prefs.getString("username","noname"));
         logout = findViewById(R.id.logout_button);
+        userImage = findViewById(R.id.usr_image);
+        LoadImage("https://zvukogram.com/upload/cimg-1-1610623877.jpg",userImage);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,4 +89,43 @@ public class ProfileActivity extends AppCompatActivity {
         removeCurrentUserContext();
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
     }
+
+    public static void LoadImage(String url, ImageView imageView){
+        SuggestionsActivity.ImageViewHelper viewHelper = new SuggestionsActivity.ImageViewHelper();
+        try {
+            Bitmap bitmap = viewHelper.execute(url).get();
+            imageView.setImageBitmap(bitmap);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class ImageViewHelper extends AsyncTask<String,Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            Bitmap bitmap = null;
+            URL url ;
+            HttpURLConnection httpURLConnection;
+            InputStream inputStream;
+
+            try{
+                url = new URL(strings[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                inputStream=httpURLConnection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+
+            }catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return  bitmap;
+        }
+    }
+
+
 }
